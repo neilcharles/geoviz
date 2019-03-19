91228c3792f8e2188c2778737bb69ecb2a623688
Neil Charles <neil.charles@mediacom.com>
2019-03-19 11:21:38 +0000

    alpha option in ggslippy()

--- R/ggslippy.R
+++ R/ggslippy.R
@@ -1,15 +1,16 @@
 #' Creates an overlay image from various sources using Miles McBain's slippymath
 #'
 #' @param slippy_raster A raster raster returned by either \code{slippy_raster()} or \code{slippy_overlay(return_png = FALSE)}
+#' @param alpha Opacity of the raster in ggplot
 #'
 #' @return a ggplot object
 #'
 #' @examples
 #' \donttest{
-#' example...
+#'
 #' }
 #' @export
-ggslippy <- function(slippy_raster){
+ggslippy <- function(slippy_raster, alpha = 1){
 
   image_df <- raster::as.data.frame(slippy_raster, xy = TRUE)
 
@@ -18,7 +19,7 @@ ggslippy <- function(slippy_raster){
   image_df$hex <- grDevices::rgb(image_df$red, image_df$green, image_df$blue, maxColorValue = 255)
 
   list(
-    ggplot2::geom_raster(data = image_df, ggplot2::aes_string(x = "x", y = "y", fill = "hex"), alpha = 1),
+    ggplot2::geom_raster(data = image_df, ggplot2::aes_string(x = "x", y = "y", fill = "hex"), alpha = alpha),
     ggplot2::coord_equal(),
     ggplot2::scale_fill_identity()
   )
